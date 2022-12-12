4bcf15d0beff539566d123ca6f02b0e50ecce6d0
Artiom Nazarchuk <sel_it@tut.by>
2022-12-12 15:07:22 +0300

    Django добавление комментариев

--- Artiom_2022/blog/views.py
+++ Artiom_2022/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Post, Comment
+from .forms import CommentForm
 
 def blog_index(request):
     posts = Post.objects.order_by('-created_on')
@@ -11,9 +12,32 @@ def blog_index(request):
 def blog_detail(request, pk):
     post = Post.objects.get(pk=pk)
     comments = Comment.objects.filter(post=post)
+    form = CommentForm()
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment(
+                author=form.cleaned_data['author'],
+                body=form.cleaned_data['body'],
+                post=post
+            )
+            comment.save()
     context = {
         'post': post,
-        'comments': comments
+        'comments': comments,
+        'form': form,
     }
     return render(request, 'blog_detail.html', context)
+
+def blog_category(request, category):
+    posts = Post.objects.filter(
+        categories__name__contains=category
+    ).order_by(
+        '-created_on'
+    )
+    context = {
+        "category": category,
+        "posts": posts
+    }
+    return render(request, "blog_category.html", context)
 
